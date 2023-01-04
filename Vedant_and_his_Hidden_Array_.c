46ce1f878ea7bc1e629d7ae41f81c4f27b825495
VENKATAPOORNIKADEVI <121076147+VENKATAPOORNIKADEVI@users.noreply.github.com>
2023-01-04 17:17:48 +0530

    #include<stdio.h> int main(){     int i,n;     scanf("%d",&n);     for (i=0;i<n;i++)     {        int a,b;        scanf("%d%d",&a,&b);        if(a==1)        {            if(b%2==0)            {                printf("Even ");            }            else            {                printf ("Odd ");            }        }        else if(b%2==0)        {            printf("Impossible ");        }        else if(a%2==0)        {            printf("Even ");        }        else        {            printf("Odd ");        }     } }

---
+++ Vedant_and_his_Hidden_Array_.c
@@ -0,0 +1,38 @@
+#include<stdio.h>
+int main(){
+    int i,n;
+    scanf("%d",&n);
+    for (i=0;i<n;i++)
+    {
+       int a,b;
+       scanf("%d%d",&a,&b);
+       if(a==1)
+       {
+           if(b%2==0)
+           {
+               printf("Even
+");
+           }
+           else
+           {
+               printf ("Odd
+");
+           }
+       }
+       else if(b%2==0)
+       {
+           printf("Impossible
+");
+       }
+       else if(a%2==0)
+       {
+           printf("Even
+");
+       }
+       else 
+       {
+           printf("Odd
+");
+       }
+    }
+}
