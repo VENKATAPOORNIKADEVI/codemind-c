274dce6a9c41c7f19e3d8bf191c8a16861dabe35
VENKATAPOORNIKADEVI <121076147+VENKATAPOORNIKADEVI@users.noreply.github.com>
2023-01-04 13:32:59 +0530

    #include<stdio.h> int main(){     int i,n;     long int s=0;     scanf("%d",&n);     int a[n];     for(i=0;i<n;i++)     {         scanf("%d",&a[i]);     }     for(i=0;i<n;i++)     {         s=s+a[i];     }     printf("%ld",s); }

---
+++ A_Very_Big_Sum.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main(){
+    int i,n;
+    long int s=0;
+    scanf("%d",&n);
+    int a[n];
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&a[i]);
+    }
+    for(i=0;i<n;i++)
+    {
+        s=s+a[i];
+    }
+    printf("%ld",s);
+}
