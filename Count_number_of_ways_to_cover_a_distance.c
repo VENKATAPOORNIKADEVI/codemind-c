06a655eb4554d3bc267d9f9931a35f0a288ba08b
VENKATAPOORNIKADEVI <121076147+VENKATAPOORNIKADEVI@users.noreply.github.com>
2023-01-05 20:23:59 +0530

    #include<stdio.h> int  f(int n) {     if(n<0)     {          return 0;     }     if(n==0)     {         return 1;     }     return f(n-1)+f(n-2)+f(n-3); } int main(){     int n;     scanf("%d",&n);     printf("%d",f(n)); }

---
+++ Count_number_of_ways_to_cover_a_distance.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int  f(int n)
+{
+    if(n<0)
+    {
+         return 0;
+    }
+    if(n==0)
+    {
+        return 1;
+    }
+    return f(n-1)+f(n-2)+f(n-3);
+}
+int main(){
+    int n;
+    scanf("%d",&n);
+    printf("%d",f(n));
+}
