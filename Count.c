25ffd97923753b4899e59bd1f7a40c675374d90f
VENKATAPOORNIKADEVI <121076147+VENKATAPOORNIKADEVI@users.noreply.github.com>
2023-01-06 19:26:06 +0530

    #include<stdio.h> int main(){     int i,n,c=0,b=0;     scanf("%d",&n);     int a[n];     for(i=0;i<n;i++)     {         scanf("%d",&a[i]);     }     for(i=0;i<n;i++)     {         if(a[i]%2==0)         {             c++;         }         else         {             b++;         }     }     printf("%d %d",c,b); }

---
+++ Count.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+int main(){
+    int i,n,c=0,b=0;
+    scanf("%d",&n);
+    int a[n];
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&a[i]);
+    }
+    for(i=0;i<n;i++)
+    {
+        if(a[i]%2==0)
+        {
+            c++;
+        }
+        else
+        {
+            b++;
+        }
+    }
+    printf("%d %d",c,b);
+}
