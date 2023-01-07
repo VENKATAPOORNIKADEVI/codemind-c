05bd25fd8d2dcea17c9d74d20e8227ce508a6d04
VENKATAPOORNIKADEVI <121076147+VENKATAPOORNIKADEVI@users.noreply.github.com>
2023-01-07 09:35:43 +0530

    #include<stdio.h> int main(){     int n,m,i,j,a[10][10],s=0;     scanf("%d%d",&n,&m);     for(i=0;i<n;i++)     {         for(j=0;j<m;j++)         {             scanf("%d",&a[i][j]);         }     }      for(i=0;i<n;i++)     {         for(j=0;j<m;j++)         {            if(i==0 || i==n-1 || j==0 || j==m-1)            {                s+=a[i][j];            }         }     }     printf("%d",s); }

---
+++ Sum_of_all_boundary_values_in_a_matrix.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+int main(){
+    int n,m,i,j,a[10][10],s=0;
+    scanf("%d%d",&n,&m);
+    for(i=0;i<n;i++)
+    {
+        for(j=0;j<m;j++)
+        {
+            scanf("%d",&a[i][j]);
+        }
+    }
+     for(i=0;i<n;i++)
+    {
+        for(j=0;j<m;j++)
+        {
+           if(i==0 || i==n-1 || j==0 || j==m-1)
+           {
+               s+=a[i][j];
+           }
+        }
+    }
+    printf("%d",s);
+}
