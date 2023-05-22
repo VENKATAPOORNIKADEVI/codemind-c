586834a8bd4df863657fb19b779703c2904b61f6
VENKATAPOORNIKADEVI <121076147+VENKATAPOORNIKADEVI@users.noreply.github.com>
2023-05-22 14:53:37 +0530

    #include<stdio.h> int main() {     int n,d,i=0,j;     scanf("%d",&n);     int a[n];     while(n!=0)     {         d=n%10;         n=n/10;         a[i]=d;         i++;     }     for(j=i-1;j>=0;j--)     {         if(a[j]==6)         {             a[j]=9;             break;         }
        }
        for(j=i-1;j>=0;j--)
        {
            printf("%d",a[j]);
        }
    }

--- Maximum_69_Number.c
+++ Maximum_69_Number.c
@@ -1,15 +1,16 @@
 #include<stdio.h>
-int main(){
-        int n,d,i=0,j;
-        scanf("%d",&n);
-        int a[n];
-        while(n!=0)
-        {
-            d=n%10;
-            n=n/10;
-            a[i]=d;
-            i++;
-        }
+int main()
+{
+    int n,d,i=0,j;
+    scanf("%d",&n);
+    int a[n];
+    while(n!=0)
+    {
+        d=n%10;
+        n=n/10;
+        a[i]=d;
+        i++;
+    }
     for(j=i-1;j>=0;j--)
     {
         if(a[j]==6)
@@ -17,6 +18,7 @@ int main(){
             a[j]=9;
             break;
         }
+        
     }
     for(j=i-1;j>=0;j--)
     {
